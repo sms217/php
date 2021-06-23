6476be3e685883783cb3b1248e9afdee9b18516a
sms217 <minsung0316@naver.com>
2021-06-23 14:05:26 +0900

    hh

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <body>
+        <?php 
+            echo date('Y-m-d H:i:s');
+            $string = "gggggaga";
+            echo "<br>";
+            echo "길이는 ".strlen($string);
+
+            $a = 10;
+            $b = "10";
+            echo "<br>";
+            echo $a+$b;
+            // echo "gaga";
+        ?>
+    </body>
+</html>
